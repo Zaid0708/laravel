b98d845e1ea70b51b9bde51f534e8a856fcb7b1f
Zaid0708 <159281410+Zaid0708@users.noreply.github.com>
2024-08-22 14:46:05 +0300

    new db

--- database/migrations/2024_08_21_110244_create_users_table.php
+++ database/migrations/2024_08_21_110244_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->foreignId('role_id')->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
